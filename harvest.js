72b9225e45235a73a4fa6767093ef1287a53bad5
Midge 't Hoen <midgethoen@gmail.com>
2016-03-05 12:26:40 +0100

    format

--- harvest.js
+++ harvest.js
@@ -1,14 +1,13 @@
 module.exports = function (creep) {
-
-	if(creep.carry.energy < creep.carryCapacity) {
-		var sources = creep.room.find(FIND_SOURCES);
-		if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-			creep.moveTo(sources[0]);
-		}
-	}
-	else {
-		if(creep.transfer(Game.spawns.Spawn1, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-			creep.moveTo(Game.spawns.Spawn1);
-		}
-	}
+  if(creep.carry.energy < creep.carryCapacity) {
+    var sources = creep.room.find(FIND_SOURCES)
+    if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+      creep.moveTo(sources[0])
+    }
+  }
+  else {
+    if(creep.transfer(Game.spawns.Spawn1, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+      creep.moveTo(Game.spawns.Spawn1)
+    }
+  }
 }
