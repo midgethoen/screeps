3752e1312864e87bb88ca8238c157155ca4346d9
Midge 't Hoen <midgethoen@gmail.com>
2017-05-29 23:23:27 +0200

    change creep body

--- src/main.js
+++ src/main.js
@@ -13,7 +13,7 @@ require('./room-helpers')
 module.exports.loop = function loop() {
   R.values(Game.spawns).forEach(function (spawn) {
     if (spawn.energy === 300) {
-      spawn.createCreep(['move', 'carry', 'work', 'move', 'carry'])
+      spawn.createCreep(['work', 'move', 'carry', 'work'])
     }
   })
 
